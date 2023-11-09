b8021d292a2b40c16682ef7a1fac4912ce125794
Medeni Aba <medeniabax@gmail.com>
2023-11-09 14:18:54 +0300

    Proje dosyası ekle.

---
+++ Plakadan Şehir/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+
+Console.WriteLine("Plaka kodu gir ve şehir gelsin oyunumuza hoş geldiniz ");
+
+int a;
+Boolean b = true ;
+
+
+
+string [] şehirler = {
+    "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin", "Aydın", "Balıkesir",
+    "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli",
+    "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari",
+    "Hatay", "Isparta", "Mersin", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir",
+    "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir",
+    "Niğde", "Ordu", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat",
+    "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman",
+    "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce"
+};
+
+
+a:Console.WriteLine("1-81 arasında bir tam sayı değeri giriniz :");
+ a = int.Parse(Console.ReadLine());
+      if (0 < a && a < 82)
+      {
+          Console.WriteLine(şehirler[a-1]);
+      
+      }
+      else Console.WriteLine("Hatalı değer girdiniz");
+
+goto a;
+      
+
+
